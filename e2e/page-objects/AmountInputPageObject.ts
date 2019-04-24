06705d2c9ded5a02a336a76687d4d64fc266ac01
Newton <newton.beck@gmail.com>
2019-04-24 16:21:49 -0300

    Fixes e2e tests

--- e2e/page-objects/AmountInputPageObject.ts
+++ e2e/page-objects/AmountInputPageObject.ts
@@ -4,15 +4,16 @@ import { QRCodePageObject } from './QRCodePageObject';
 export class AmountInputPageObject {
 
   public async assertIsVisible() {
-    expect(element(by.id('AmountInputScreen'))).toBeVisible();
+    await expect(element(by.id('AmountInputScreen'))).toBeVisible();
   }
 
   public async fillAmount(amount: number) {
+    await element(by.id('amountInput')).tap();
     await element(by.id('amountInput')).typeText(`${amount}`);
   }
 
   public async assertHasAmountOf(amount: number) {
-    expect(element(by.text(`${amount}`))).toBeVisible();
+    await expect(element(by.text(`${amount}`))).toBeVisible();
   }
 
   public async confirm(): Promise<QRCodePageObject> {
--- e2e/page-objects/QRCodePageObject.ts
+++ e2e/page-objects/QRCodePageObject.ts
@@ -3,15 +3,15 @@ import { by, element, expect } from 'detox';
 export class QRCodePageObject {
 
   public async assertIsVisible() {
-    expect(element(by.id('QRCodeScreen'))).toBeVisible();
+    await expect(element(by.id('QRCodeScreen'))).toBeVisible();
   }
 
   public async share() {
-    await element(by.id('shareBtn')).tap();
+    await element(by.id('shareButton')).tap();
   }
 
   public async assertIsShared() {
-    expect(element(by.text('OK'))).toBeVisible();
+    await expect(element(by.text('OK'))).toBeVisible();
   }
 
 }
